a6f9e2a511e138c65c87ae41bf74a432e5b97556
rajeshmurmu <rajeshsonmurmur2017@gmail.com>
2026-02-04 00:18:33 +0530

    populate user address to response

--- app/api/(authenticated)/admin/users/route.ts
+++ app/api/(authenticated)/admin/users/route.ts
@@ -10,31 +10,32 @@ export async function GET() {
     if (!session?.user?.role || session.user.role !== "admin") {
       return NextResponse.json(
         { error: "Unauthorized", message: "Access Denied" },
-        { status: 401 }
+        { status: 401 },
       );
     }
 
     const users = await User.find()
       .populate("role")
       .populate("orders")
+      .populate("address")
       .lean({ timeout: 30000 });
 
     if (!users) {
       return NextResponse.json(
         { success: false, message: "No users found" },
-        { status: 404 }
+        { status: 404 },
       );
     }
 
     return NextResponse.json(
       { success: true, users, message: "Users fetched successfully" },
-      { status: 200 }
+      { status: 200 },
     );
   } catch (error) {
     console.error("Error fetching users:", error);
     return NextResponse.json(
       { success: false, message: "Internal Server Error" },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
